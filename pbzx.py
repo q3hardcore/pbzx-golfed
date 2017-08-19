c85c8cb389172a941e4f3e77d36821e0be5ceb41
q3hardcore <q3hardcore@users.noreply.github.com>
2017-08-19 22:02:44 +1000

    Python 3.3+ port revised

--- pbzx.py
+++ pbzx.py
@@ -1,5 +1,4 @@
-import lzma,struct,sys;a=sys.argv;u=struct.unpack;r=open(a[1],'rb').read;w=open(a[2],'wb').write;r(12);b=r(8)
+import lzma,struct,sys;x,y,z=sys.argv;u=struct.unpack;r=open(y,'rb').read;w=open(z,'wb').write;r(12);b=r(8)
 while b:
- l=u('>Q',b);s=u('>Q',r(8));d=r(s[0])
- if l>s:d=lzma.decompress(d)
- w(d);b=r(8)
+ l,=u('>Q',b);s,=u('>Q',r(8));d=r(s)
+ if l>s:d=lzma.decompress(d);w(d);b=r(8)
