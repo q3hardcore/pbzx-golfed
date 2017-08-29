0ba95611a7622dc99db30db1d91014984acd62f7
q3hardcore <q3hardcore@users.noreply.github.com>
2017-08-29 17:45:49 +1000

    Add files via upload
    Fixed #1
    If statements need their own line.

--- pbzx.py
+++ pbzx.py
@@ -1,4 +1,5 @@
-import lzma,struct,sys;x,y,z=sys.argv;u=struct.unpack;r=open(y,'rb').read;w=open(z,'wb').write;r(12);b=r(8)
+import lzma,struct,sys;x,y,z=sys.argv;u=struct.unpack;r=open(y,'rb').read;w=open(z,'wb').write;r(12);b=r(16)
 while b:
- l,=u('>Q',b);s,=u('>Q',r(8));d=r(s)
- if l>s:d=lzma.decompress(d);w(d);b=r(8)
+ l,s=u('>QQ',b);d=r(s)
+ if l>s:d=lzma.decompress(d)
+ w(d);b=r(16)
